3dc19ba5abc64d84539f5459871acf0444d04f31
rht <rhtbot@protonmail.com>
2019-03-14 11:46:57 +0000

    Update Contract base form

--- economicsl/contract.py
+++ economicsl/contract.py
@@ -1,24 +1,32 @@
 from collections import defaultdict
 
 
-class Contract:
+class Contract(object):
     __slots__ = 'assetParty', 'liabilityParty', 'ctype'
-
     """Base class for a model contract"""
+    ctype = 'Contract'
+
+    def __init__(self, assetParty, liabilityParty):
+        self.assetParty = assetParty
+        self.liabilityParty = liabilityParty
+
     def get_asset_party(self):
-        pass
+        return self.assetParty
 
     def get_liability_party(self):
-        pass
+        return self.liabilityParty
 
     def get_value(self, me):
         pass
 
-    def get_available_actions(self, me):
+    def get_action(self, me):
         pass
 
     def get_name(self, me):
         pass
+
+    def is_eligible(self, me):
+        return False
 
 
 class Contracts(object):
